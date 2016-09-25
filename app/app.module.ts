8d824073ad0f14a71b3810f767170fa1a21add96
Ali Watters <ali.watters@gmail.com>
2016-09-25 10:18:18 -0700

    chp07 complete

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import './rxjs-extensions';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component'
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent} from './heroes.component';
+import { HeroSearchComponent } from './hero-search.component';
+import { HeroesComponent } from './heroes.component';
 
 import { HeroService } from './hero.service';
 
@@ -15,12 +23,15 @@ import { routing } from './app.routing';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     routing
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
+    HeroSearchComponent,
     HeroesComponent
   ],
   bootstrap: [AppComponent],
